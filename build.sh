688344085882d21bcd9d99d133e4318a2c718e81
Hermesss <den.hermesss@gmail.com>
2020-06-30 15:44:14 +0300

    added first time full checkout

--- build.sh
+++ build.sh
@@ -4,13 +4,19 @@ sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt update && sudo apt upgrade -y
 sudo apt install git nginx -y 
 
+#download github repo
+cd /tmp
+git clone https://github.com/Hermesss/mdt.git
+sudo mv /tmp/mdt/www/* /var/www/html
+sudo systemctl restart nginx
+
 #git sparce-checkout section
 #install required apps
 sudo apt update
 sudo apt install make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
 cd /tmp
 #download github repo
-git clone https://github.com/Hermesss/mdt.git
+#git clone https://github.com/Hermesss/mdt.git
 cd ./mdt
 #git sparce-checkout setup
 git sparse-checkout init --cone
